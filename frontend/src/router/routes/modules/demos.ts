cd7d1d6d5437b2fd34892342ad1dc6e5f0893358
wuhangfeng <nicehangfeng@foxmail.com>
2026-01-28 20:28:45 +0800

    feat: icons

--- frontend/src/router/routes/modules/demos.ts
+++ frontend/src/router/routes/modules/demos.ts
@@ -1,36 +1,197 @@
 import type { RouteRecordRaw } from 'vue-router'
 
+import { IFrameView } from '@/layouts'
 import { $t } from '@/locales'
 
 const routes: RouteRecordRaw[] = [
-  // {
-  //   meta: {
-  //     icon: 'ic:baseline-view-in-ar',
-  //     keepAlive: true,
-  //     order: 1000,
-  //     title: $t('demos.title'),
-  //   },
-  //   name: 'Demos',
-  //   path: '/demos',
-  //   children: [
-  //     {
-  //       meta: {
-  //         title: $t('demos.elementPlus'),
-  //       },
-  //       name: 'NaiveDemos',
-  //       path: '/demos/element',
-  //       component: () => import('@/views/demos/element/index.vue'),
-  //     },
-  //     {
-  //       meta: {
-  //         title: $t('demos.form'),
-  //       },
-  //       name: 'BasicForm',
-  //       path: '/demos/form',
-  //       component: () => import('@/views/demos/form/basic.vue'),
-  //     },
-  //   ],
-  // },
+  {
+    meta: {
+      icon: 'ic:baseline-view-in-ar',
+      keepAlive: true,
+      order: 1000,
+      title: $t('demos.title'),
+    },
+    name: 'Demos',
+    path: '/demos',
+    children: [
+      // 功能
+      {
+        meta: {
+          icon: 'mdi:feature-highlight',
+          title: $t('demos.features.title'),
+        },
+        name: 'FeaturesDemos',
+        path: '/demos/features',
+        children: [
+          {
+            name: 'IconsDemo',
+            path: '/demos/features/icons',
+            component: () => import('@/views/features/icons/index.vue'),
+            meta: {
+              icon: 'lucide:annoyed',
+              title: $t('demos.features.icons'),
+            },
+          },
+          // {
+          //   name: 'LoginExpiredDemo',
+          //   path: '/demos/features/login-expired',
+          //   component: () =>
+          //     import('#/views/demos/features/login-expired/index.vue'),
+          //   meta: {
+          //     icon: 'mdi:encryption-expiration',
+          //     title: $t('demos.features.loginExpired'),
+          //   },
+          // },
+          // {
+          //   name: 'WatermarkDemo',
+          //   path: '/demos/features/watermark',
+          //   component: () =>
+          //     import('#/views/demos/features/watermark/index.vue'),
+          //   meta: {
+          //     icon: 'lucide:tags',
+          //     title: $t('demos.features.watermark'),
+          //   },
+          // },
+          // {
+          //   name: 'FeatureTabsDemo',
+          //   path: '/demos/features/tabs',
+          //   component: () => import('#/views/demos/features/tabs/index.vue'),
+          //   meta: {
+          //     icon: 'lucide:app-window',
+          //     title: $t('demos.features.tabs'),
+          //   },
+          // },
+          // {
+          //   name: 'FeatureTabDetailDemo',
+          //   path: '/demos/features/tabs/detail/:id',
+          //   component: () =>
+          //     import('#/views/demos/features/tabs/tab-detail.vue'),
+          //   meta: {
+          //     activePath: '/demos/features/tabs',
+          //     hideInMenu: true,
+          //     maxNumOfOpenTab: 3,
+          //     title: $t('demos.features.tabDetail'),
+          //   },
+          // },
+          // {
+          //   name: 'HideChildrenInMenuParentDemo',
+          //   path: '/demos/features/hide-menu-children',
+          //   meta: {
+          //     hideChildrenInMenu: true,
+          //     icon: 'ic:round-menu',
+          //     title: $t('demos.features.hideChildrenInMenu'),
+          //   },
+          //   children: [
+          //     {
+          //       name: 'HideChildrenInMenuDemo',
+          //       path: '',
+          //       component: () =>
+          //         import('#/views/demos/features/hide-menu-children/parent.vue'),
+          //       meta: {
+          //         // hideInMenu: true,
+          //         title: $t('demos.features.hideChildrenInMenu'),
+          //       },
+          //     },
+          //     {
+          //       name: 'HideChildrenInMenuChildrenDemo',
+          //       path: '/demos/features/hide-menu-children/children',
+          //       component: () =>
+          //         import('#/views/demos/features/hide-menu-children/children.vue'),
+          //       meta: {
+          //         activePath: '/demos/features/hide-menu-children',
+          //         title: $t('demos.features.hideChildrenInMenu'),
+          //       },
+          //     },
+          //   ],
+          // },
+          // {
+          //   name: 'FullScreenDemo',
+          //   path: '/demos/features/full-screen',
+          //   component: () =>
+          //     import('#/views/demos/features/full-screen/index.vue'),
+          //   meta: {
+          //     icon: 'lucide:fullscreen',
+          //     title: $t('demos.features.fullScreen'),
+          //   },
+          // },
+          // {
+          //   name: 'FileDownloadDemo',
+          //   path: '/demos/features/file-download',
+          //   component: () =>
+          //     import('#/views/demos/features/file-download/index.vue'),
+          //   meta: {
+          //     icon: 'lucide:hard-drive-download',
+          //     title: $t('demos.features.fileDownload'),
+          //   },
+          // },
+          // {
+          //   name: 'ClipboardDemo',
+          //   path: '/demos/features/clipboard',
+          //   component: () =>
+          //     import('#/views/demos/features/clipboard/index.vue'),
+          //   meta: {
+          //     icon: 'lucide:copy',
+          //     title: $t('demos.features.clipboard'),
+          //   },
+          // },
+          // {
+          //   name: 'MenuQueryDemo',
+          //   path: '/demos/menu-query',
+          //   component: () =>
+          //     import('#/views/demos/features/menu-query/index.vue'),
+          //   meta: {
+          //     icon: 'lucide:curly-braces',
+          //     query: {
+          //       id: 1,
+          //     },
+          //     title: $t('demos.features.menuWithQuery'),
+          //   },
+          // },
+          // {
+          //   name: 'NewWindowDemo',
+          //   path: '/demos/new-window',
+          //   component: () =>
+          //     import('#/views/demos/features/new-window/index.vue'),
+          //   meta: {
+          //     icon: 'lucide:app-window',
+          //     openInNewWindow: true,
+          //     title: $t('demos.features.openInNewWindow'),
+          //   },
+          // },
+          // {
+          //   name: 'VueQueryDemo',
+          //   path: '/demos/features/vue-query',
+          //   component: () =>
+          //     import('#/views/demos/features/vue-query/index.vue'),
+          //   meta: {
+          //     icon: 'lucide:git-pull-request-arrow',
+          //     title: 'Tanstack Query',
+          //   },
+          // },
+          // {
+          //   name: 'RequestParamsSerializerDemo',
+          //   path: '/demos/features/request-params-serializer',
+          //   component: () =>
+          //     import('#/views/demos/features/request-params-serializer/index.vue'),
+          //   meta: {
+          //     icon: 'lucide:git-pull-request-arrow',
+          //     title: $t('demos.features.requestParamsSerializer'),
+          //   },
+          // },
+          // {
+          //   name: 'BigIntDemo',
+          //   path: '/demos/features/json-bigint',
+          //   component: () =>
+          //     import('#/views/demos/features/json-bigint/index.vue'),
+          //   meta: {
+          //     icon: 'lucide:grape',
+          //     title: 'JSON BigInt',
+          //   },
+          // },
+        ],
+      },
+    ],
+  },
 ]
 
 export default routes
---
+++ frontend/src/views/features/icons/index.vue
@@ -0,0 +1,120 @@
+<script lang="ts" setup>
+import { h, ref } from 'vue'
+
+import { IconPicker, Page } from '@/packages/effects/common-ui/src'
+import {
+  MdiKeyboardEsc,
+  SvgAvatar1Icon,
+  SvgAvatar2Icon,
+  SvgAvatar3Icon,
+  SvgAvatar4Icon,
+  SvgBellIcon,
+  SvgCakeIcon,
+  SvgCardIcon,
+  SvgDownloadIcon,
+  SvgGithubIcon,
+  SvgGoogleIcon,
+  SvgQQChatIcon,
+  SvgWeChatIcon,
+} from '@/packages/icons'
+
+import { ElCard, ElInput } from 'element-plus'
+import 'element-plus/es/components/card/style/css'
+import 'element-plus/es/components/input/style/css'
+
+const iconValue1 = ref('ant-design:trademark-outlined')
+const iconValue2 = ref('svg:avatar-1')
+const iconValue3 = ref('mdi:alien-outline')
+const iconValue4 = ref('mdi-light:book-multiple')
+
+// const inputComponent = h(ElInput, {
+//   placeholder: '请输入图标名称',
+// })
+const inputComponent = ElInput
+</script>
+
+<template>
+  <Page title="图标">
+    <template #description>
+      <div class="mt-2 text-foreground/80">
+        图标可在
+        <a
+          class="text-primary"
+          href="https://icon-sets.iconify.design/"
+          target="_blank"
+        >
+          Iconify
+        </a>
+        中查找，支持多种图标库，如 Material Design, Font Awesome, Jam Icons 等。
+      </div>
+    </template>
+
+    <ElCard class="mb-5" header="Iconify">
+      <div class="flex items-center gap-5">
+        <SvgGithubIcon class="size-8" />
+        <SvgGoogleIcon class="size-8" />
+        <SvgQQChatIcon class="size-8" />
+        <SvgWeChatIcon class="size-8" />
+        <MdiKeyboardEsc class="size-8" />
+      </div>
+    </ElCard>
+
+    <ElCard class="mb-5" header="Svg Icons">
+      <div class="flex items-center gap-5">
+        <SvgAvatar1Icon class="size-8" />
+        <SvgAvatar2Icon class="size-8 text-red-500" />
+        <SvgAvatar3Icon class="size-8 text-green-500" />
+        <SvgAvatar4Icon class="size-8" />
+        <SvgCakeIcon class="size-8" />
+        <SvgBellIcon class="size-8" />
+        <SvgCardIcon class="size-8" />
+        <SvgDownloadIcon class="size-8" />
+      </div>
+    </ElCard>
+
+    <ElCard class="mb-5" header="Tailwind CSS">
+      <div class="flex items-center gap-5 text-3xl">
+        <span class="icon-[ant-design--alipay-circle-outlined]"></span>
+        <span class="icon-[ant-design--account-book-filled]"></span>
+        <span class="icon-[ant-design--container-outlined]"></span>
+        <span class="icon-[svg-spinners--wind-toy]"></span>
+        <span class="icon-[svg-spinners--blocks-wave]"></span>
+        <span class="icon-[line-md--compass-filled-loop]"></span>
+      </div>
+    </ElCard>
+
+    <ElCard class="mb-5" header="图标选择器">
+      <div class="mb-5 flex items-center gap-5">
+        <span>原始样式(Iconify):</span>
+        <IconPicker v-model="iconValue1" class="w-[200px]" />
+      </div>
+      <div class="mb-5 flex items-center gap-5">
+        <span>原始样式(svg):</span>
+        <IconPicker v-model="iconValue2" class="w-[200px]" prefix="svg" />
+      </div>
+      <div class="mb-5 flex items-center gap-5">
+        <span>自定义Input:</span>
+        <IconPicker
+          :input-component="inputComponent"
+          v-model="iconValue3"
+          icon-slot="addonAfter"
+          model-value-prop="value"
+          prefix="mdi"
+        />
+      </div>
+      <div class="flex items-center gap-5">
+        <span>显示为一个Icon:</span>
+        <ElInput
+          v-model:value="iconValue4"
+          allow-clear
+          placeholder="点击这里选择图标"
+          style="width: 300px"
+        >
+          <template #addonAfter>
+            <IconPicker v-model="iconValue4" prefix="mdi-light" type="icon" />
+          </template>
+        </ElInput>
+      </div>
+    </ElCard>
+  </Page>
+</template>
